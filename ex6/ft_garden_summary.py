95ba3e4556013d84bc05655010a2e78a8acb1279
josjimen <pepojimenezbethencourt@gmail.com>
2026-01-29 16:52:12 +0100

    ex6

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,6 @@
+def ft_garden_summary():
+    grdn_name = input("Enter garden name: ")
+    nmbr_plants = input("Enter number of plants: ")
+    print(f"Garden: {grdn_name}")
+    print(f"Plants: {nmbr_plants}")
+    print("Status: Growing well!")
